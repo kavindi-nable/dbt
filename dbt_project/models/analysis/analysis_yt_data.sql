f7cd9de6c029cfe33016e84517740c9bad38a3f5
Kavindi Mudunkotuwa <kavindim@n-able.biz>
2023-07-11 09:57:26 +0530

    Added the changes to tranformation and analysis models

--- dbt_project/models/analysis/analysis_yt_data.sql
+++ dbt_project/models/analysis/analysis_yt_data.sql
@@ -1,11 +1,11 @@
 {{ config( materialized='table', table_name='analysis_yt_data' ) }}
 
 SELECT
-    GENRE,
-    COUNT(A) as total,
-    MAX(A) as maximum,
-    MIN(A) as minimum, 
-    AVG(A) as average
+    (SELECT COUNT(*) FROM {{ ref('yt_model') }}) AS total_count,
+    (SELECT AVG(total) FROM {{ ref('yt_model') }}) AS mean,
+    (SELECT MIN(total) FROM {{ ref('yt_model') }}) AS min_value,
+    (SELECT MAX(total) FROM {{ ref('yt_model') }}) AS max_value
+
     
-FROM YT
-GROUP BY GENRE
+FROM {{ ref('yt_model') }}
+GROUP BY 1
--- dbt_project/models/transformation/yt_model.sql
+++ dbt_project/models/transformation/yt_model.sql
@@ -1,6 +1,6 @@
 {{config(materialized = 'table', table_name = 'yt_model')}}
 SELECT 
-    GENRE,    
-    COUNT(A) as total
-FROM YT
-Group BY GENRE
+    "Genre",    
+    COUNT('ID') as total
+FROM {{ source('yt_csv', 'yt') }}
+Group BY 1
